b23bbb4fd3f767c8411ff64ff4d3c827e33a64d4
Nam Lê Hải <Nam@nams--macbook-pro.local>
2017-11-25 13:57:42 +0700

    Initial Commit

---
+++ Practice Array/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Practice Array
+//
+//  Created by Nam 😋 on 11/25/17.
+//  Copyright © 2017 Nam 😋. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
